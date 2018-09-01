096272bf4563dcbad8da4bfc87316db4a689f150
Swapnik Katkoori <katkoor2@msu.edu>
2018-09-01 15:27:01 -0400

    heroku socket.io config

--- server.js
+++ server.js
@@ -20,6 +20,10 @@ app.use(bodyParser.json());
 
 app.get("/",(req,res)=>console.log("working"))
 
+io.configure(function () { 
+    io.set("transports", ["xhr-polling"]); 
+    io.set("polling duration", 10); 
+});
 
 io.on('connection',(socket)=>{
 	console.log("socket ",socket.id, "is connected")
